9061f98a628af61e122aa365c7ca5885ffaf5605
Manuel Miranda <3057476220301@ingenieria.usac.edu.gt>
2020-03-17 15:00:05 -0600

    Menu Principal
    Creado menu principal y cambio de comentarios

--- macros.asm
+++ macros.asm
@@ -1,16 +1,15 @@
-; Archivo que contendra los macros utilizados
-; para la practica 4 de arqui. Esto para tener un orden mejor en el codigo
+; FILE THAT CONTAINS ALL THE MACROS FOR THE PROGRAM
 
-; Macro para imprimir
+; PRINT ON THE SCREEN
 print macro string
     mov ax, @data
     mov ds, ax
-    mov ah, 09h             ; Numero de funcion para imprimir cadena en pantalla
-    mov dx, offset string   ; Equivalente a lea dx, cadena. Inicializa en dx la posicion donde comienza la cadena
+    mov ah, 09h             ; PRINT
+    mov dx, offset string
     int 21h
 endm
 
-; Macro para leer caracter
+; GET CHARACTER
 getChar macro
     mov ah, 01h
     int 21h
--- main.asm
+++ main.asm
@@ -2,23 +2,47 @@ include macros.asm
 
 .model small
 
-; SEGMENTO DE PILA
+; STACK SEGMENT
 .stack
 
-; SEGMENTO DE DATO
-; En este segmento se deben de declarar todas las "variables" a utilizar.
+; DATA SEGMENT
+; ON THIS SEGMENT WE CREATE THE "VARIABLES" THAT WE ARE GOING TO USE IN THE PROGRAM
 .data
 
-; SEGMENTO DE CODIGO
-; Parte del codigo assembler donde se coloca el codigo
+    ; HEADER AND MENU
+header db 9, 9, 'UNIVERSIDAD DE SAN CARLOS DE GUATEMALA', 13, 10, 9, 9, 'FACULTAD DE INGENIERIA', 13, 10, 9, 9, 'CIENCIAS Y SISTEMAS', 13, 10, 9, 9, 'ARQUITECTURA DE COMPUTADORES Y ENSAMBLADORES 1', 13, 10, 9, 9, 'NOMBRE: ANGEL MANUEL MIRANDA ASTURIAS', 13, 10, 9, 9, 'CARNET: 201807394', 13, 10, 9, 9, 'SECCION: A', 13, 10, '$'
+menu db 13, 10, 9, 9, '-_-MENU-_-', 13, 10, 9, 9, '1) Iniciar Juego', 13, 10, 9, 9, '2) Cargar Juego', 13, 10, 9, 9, '3) Salir', 13, 10, '$'
+    ; END HEADER AND MENU
+
+; CODE SEGMENT
+; ON THIS SEGMENT WE START TO WRITE THE CODE
 .code
 
 main proc
 
-    practica4:
+    ; BEGGINIG OF THE PROGRAM
+    Start:
+        PrincipalMenu
 
-    Salir:
-        mov ah, 4ch     ; Numero de funcion para finalizar el programa
+    ; PRINCIPAL MENU
+    PrincipalMenu:
+        print header
+        print menu
+        getChar
+        ; COMPARE THE CHAR THAT THE USER WRITE IN THE PROGRAM
+        cmp al, '1'         ; START GAME
+            je Game
+        cmp al, '2'         ; LOAD GAME
+            je LoadGame
+        cmp al, '3'         ; EXIT
+            je Exit
+    
+    Game:
+
+    LoadGame:
+
+    Exit:
+        mov ah, 4ch     ; END PROGRAM
         xor al, al
         int 21h
 
